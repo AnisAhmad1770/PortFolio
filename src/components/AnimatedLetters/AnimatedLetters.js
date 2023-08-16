2b680069ddecd36edf09cc76d0a3bc1af9228929
Anis Ahmad <anisahmad1770@gmail.com>
2023-08-16 18:00:40 +0500

    Commit4

--- src/components/AnimatedLetters/AnimatedLetters.js
+++ src/components/AnimatedLetters/AnimatedLetters.js
@@ -1,18 +1,16 @@
+// AnimatedLetters.jsx
 import "./AnimatedLetters.scss";
 
-const AnimatedLetters = ({letterClass,strArray,idx}) => {
+const AnimatedLetters = ({ letterClass, strArray, idx }) => {
   return (
     <span>
-      {
-        strArray.map((char,i)=>(
-            <span key={char+i} className={'$letterClass} _${i+idx}'}>
-                {char}
-            </span>
-
-        ))
-      }
+      {strArray.map((char, i) => (
+        <span key={char + i} className={`${letterClass} _${i + idx}`}>
+          {char}
+        </span>
+      ))}
     </span>
-  )
-}
+  );
+};
 
-export default AnimatedLetters
+export default AnimatedLetters;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,39 +1,44 @@
- import "./Home.scss"
- import { Link } from 'react-router-dom'
-import LogoTitle from '../../assets/images/logo-s.png'
-import { useState } from "react"
-import AnimatedLetters from "../AnimatedLetters/AnimatedLetters"
-const Home = () => {
-  const[letterClass,setLetterClass]=useState('text-animate')
-  const nameArray=['l','o','b','o','d','a','m']
-  const jobArray=['w','e','b','d','e','v','o','l','o','p','e','r','.',]
+// Home.jsx
+import "./Home.scss";
+import { Link } from 'react-router-dom';
+import LogoTitle from '../../assets/images/logo-s.png';
+import { useEffect, useState } from "react";
+import AnimatedLetters from "../AnimatedLetters/AnimatedLetters";
+import Logo from "./Logo/Logo";
 
+const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate');
+  const nameArray = ['n', 'i', 's', ' ', 'A', 'h', 'm', 'a' ,'d'];
+  const jobArray = ['w', 'e', 'b', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r', '.'];
+  useEffect(() => {
+    const idTimeOut = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+
+    return () => clearTimeout(idTimeOut);
+  }, [])
+ 
   
-    return (
+  return (
     <div className="container home-page">
       <div className="text-zone">
-
         <h1>
-        <span className={'letterClass'}>H</span>
-        <span className={'${letterClass} _12'}>I</span>
-            <br/> 
-            <span className={'${letterClass} _13'}>I</span>
-            <span className={'${letterClass} _14'}>'m</span>
-        <img src={LogoTitle } alt="developer"/>
-        <AnimatedLetters letterClass={letterClass}
-        strArray={nameArray}
-        idx={15}/>
-        
-        <br/>
-        <AnimatedLetters letterClass={letterClass}
-        strArray={jobArray}
-        idx={22}/>
-      </h1>
-      <h2>Frontend Developer/JavaScript Expert /Youtuber /Gamer</h2>
-      <Link to="/contact" className='flat-button'>Contact me</Link>
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i</span>
+          <br/>
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
+          <img src={LogoTitle} alt="developer"/>
+          <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15}/>
+          <br/>
+          <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={23}/>
+        </h1>
+        <h2>Frontend Developer/JavaScript Expert/Youtuber/Gamer</h2>
+        <Link to="/contact" className='flat-button'>Contact me</Link>
       </div>
+      <Logo/>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
